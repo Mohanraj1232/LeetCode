d4d216a8dd566094c6a08f04d22c2095fec7cca3
Mohanraj <g.mohanrajgtmt@gmail.com>
2025-07-25 21:46:15 +0530

    Solutions for Power Product/Minimum Coin Amount Impossible

--- Elite-Hope-Test-25-7-25/Minimum Coin Amount Impossible/Solution.java
+++ Elite-Hope-Test-25-7-25/Minimum Coin Amount Impossible/Solution.java
@@ -1,5 +1,31 @@
 import java.util.*;
 
 public class Solution {
-    
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+
+        int N =sc.nextInt();
+
+        int DP=0;
+        int coins[] = new int[N];
+
+        for(int i=0;i<N;i++){
+            coins[i] = sc.nextInt();
+        }
+        
+        Arrays.sort(coins);
+
+        for(int i=0;i<N;i++){
+
+            if(coins[i] - 1 >DP){
+                System.out.println(DP + 1);
+                return;
+            }
+            DP += coins[i];
+        }
+
+        System.out.println(DP + 1);
+
+        sc.close();
+    }
 }
--- Elite-Hope-Test-25-7-25/Power Product/Solution.java
+++ Elite-Hope-Test-25-7-25/Power Product/Solution.java
@@ -1,5 +1,30 @@
 import java.util.*;
 
 public class Solution {
-    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        long Num = sc.nextLong();
+        int n=sc.nextInt();
+        
+        PriorityQueue<Long> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int i=0;i<n;i++){
+            pq.add(sc.nextLong());
+        }
+
+        for(int i=0;i<n;i++){
+            long val=pq.poll();
+
+            if(val==1) continue;
+
+            while(Num % val == 0) {
+                Num /= val;
+            }
+        }
+
+        System.out.println(Num == 1 ? "Yes" : "No");
+
+    }
 }
