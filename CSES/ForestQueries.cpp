fa3a4fd647b0dd458f5bb25f6878d9f5636b66d7
Mohanraj1232 <g.mohanrajgtmt@gmail.com>
2025-10-08 19:39:37 +0530

    ForestQueries

--- CSES/ForestQueries.cpp
+++ CSES/ForestQueries.cpp
@@ -1,27 +1,38 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-#define ll long long int
 
 int main() {
+	
+	int N ,Q;
+	cin >> N >> Q;
+	int mat[N + 1][N + 1];
 
-    int N, Q;
-
-    cin >> N >> Q;
-
-    int mat[N][N];
-    char ch;
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
-            cin>>ch;
-            if(ch == '*'){
-                mat[i][j] = 1;
-            }else{
-                mat[i][j] = 0;
-            }
-        }
+    for(int r = 0; r <= N; r++){
+        mat[r][0] = 0;
+    }
+    for(int c = 0; c <= N; c++){
+        mat[0][c] = 0;
     }
 
-    for(int i = 0; i < N; i++){
+	for(int r = 1; r <= N; r++){
+	    for(int c = 1; c <= N; c++){
+	        char ch;
+	        cin >> ch;
+	        mat[r][c] = ((ch == '*') ? 1 : 0) + mat[r][c - 1];
+	    }
+	}
+
+	for(int c = 1; c <= N; c++){
+	    for(int r = 1; r <= N; r++){
+	        mat[r][c] += mat[r - 1][c];
+	    }
+	}
+    
+    while(Q--){
+        int r1 ,r2 ,c1 ,c2;
+        cin >> r1 >> c1 >> r2 >> c2;
         
+        cout << mat[r2][c2] - mat[r2][c1 - 1] - mat[r1 - 1][c2] + mat[r1 - 1][c1 - 1] << endl;
     }
+
 }
